a36e596e3fca38f98bf377f629d323bead36fdd3
Vinicius Nogueira <vinicius@e-deas.com.br>
2022-03-19 19:25:57 -0300

    colocado owl-carousel/ajeitado n problemas

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+var menuBtn = document.getElementById("menuBtn");
+var menuMobile = document.getElementById("menuMobile");
+
+menuMobile.style.right = "-450px";
+menuBtn.onclick = function () {
+  if (menuMobile.style.right === "-450px") {
+    menuMobile.style.right = "0";
+  } else {
+    menuMobile.style.right = "-450px";
+  }
+};
+
+$("#slide-projetos").owlCarousel({
+  loop: true,
+  margin: 5,
+  responsiveClass: true,
+  autoHeight: true,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 5,
+      nav: true,
+      loop: false,
+    },
+  },
+});
